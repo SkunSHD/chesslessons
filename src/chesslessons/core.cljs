8ad7b58d4b45764f7ca899f105585f1c5866bc07
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-02-05 18:58:12 +0200

    Add

--- src/chesslessons/core.cljs
+++ src/chesslessons/core.cljs
@@ -1,23 +1,34 @@
 (ns chesslessons.core
     (:require
-      [reagent.core :as r]
-      [chesslessons.firebase :as fb :refer [firebase]]
+		[reagent.core :as r]
+		[chesslessons.firebase :as fbs :refer [firebase]]
+;       Models
+		[chesslessons.user-model :as user_model]
 ))
 
 (def log (.-log js/console))
 
+
+
+; APP ID 1001305193295261
+; App Secret 48f41533602ddf5804d81ebbbc447d0e
+
+
 ;; -------------------------
 ;; Views
-
 (defn home-page []
-	(fb/sign_in_with_email_and_password "andiwillfly@gmail.com", "ward121314")
-  [:div [:h2 "Welcome to Reage321nt3"]])
+	(log "USER!3" (str @user_model/user))
+;	(fbs/sign_in_with_email_and_password "andiwillfly@gmail.com", "ward121314")
+  [:div
+   [:button { :onClick fbs/facebook_auth } "facebook auth"]
+   [:h2 "Welcome to Reage321nt3"]])
+
 
 ;; -------------------------
 ;; Initialize app
-
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
+
 
 (defn init! []
   (mount-root))
--- src/chesslessons/firebase.cljs
+++ src/chesslessons/firebase.cljs
@@ -1,6 +1,8 @@
 (ns chesslessons.firebase
 	(:require
 		[reagent.core :refer [atom cursor]]
+;		Models
+		[chesslessons.user-model :as user_model]
 ))
 
 (def log (.-log js/console))
@@ -9,11 +11,41 @@
 
 
 (defn auth []
-	((.-auth firebase)))
+	(.auth firebase))
 
 
 (defn sign_in_with_email_and_password [email password]
 	(.catch (.then
 		(.signInWithEmailAndPassword (auth) email password) (fn [user] (log user "user...")))
 	        (fn [e] (log (.message e)))))
+
+
+; ==================
+; Facebook
+(def facebook_auth_provider (new (.-FacebookAuthProvider (.-auth firebase))))
+
+
+(defn facebook_link_user [user credential]
+	(log user "HE2RE!!!" credential)
+	(.then (.link user credential) (fn [wtf] (log "VSE" wtf))))
+
+
+(defn facebook_fect_provider_for_email [email credential]
+	(.then (.fetchProvidersForEmail (auth) email)
+	       (fn [providers]
+		       (.then (.signInWithEmailAndPassword (auth) email "ward121314")
+		              (fn [user] (facebook_link_user user credential))))))
+
+
+(defn facebook_auth_error [error]
+	(log error)
+	(if (=(.-code error) "auth/account-exists-with-different-credential")
+		(facebook_fect_provider_for_email (.-email error) (.-credential error))))
+
+
+(defn facebook_auth []
+	(.catch (.then
+		  (.signInWithPopup (auth) facebook_auth_provider) user_model/set_user)
+	        facebook_auth_error))
+
 
