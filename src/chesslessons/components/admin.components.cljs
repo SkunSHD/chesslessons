468e562bad55e8ae1be9aa1ebb3e8c53ff7b28e9
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-02-06 18:12:43 +0200

    del .idea files

--- src/chesslessons/components/admin.components.cljs
+++ src/chesslessons/components/admin.components.cljs
@@ -4,4 +4,22 @@
 
 
 (defn render_login_form []
-	[:div "admin login form"])
+	[:form
+	 [:h1.h3.mb-3.font-weight-normal "Admin sign in"]
+	
+	 [:div.form-group
+	  [:label.sr-only "Email address"]
+	  [:input.form-control {:placeholder "Email address" :required true :type "email"}]
+	  ]
+	
+	 [:div.form-group
+	  [:label.sr-only "Password"]
+	  [:input.form-control {:placeholder "Password" :required true :type "password"}]
+	  ]
+	
+	 [:div.form-group
+	  [:img {:src "https://i.stack.imgur.com/ZW4QC.png" :onClick fbs/facebook_auth :style {:cursor "pointer"} }]
+	  ]
+	
+	 [:button.btn.btn-primary "Sign in"]
+	 ])
