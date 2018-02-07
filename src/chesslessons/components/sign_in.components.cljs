4ab08fbac41f67083d797b3694ea92cb95813fa6
malyutenko.d <malyutenko.d@gmail.com>
2018-02-07 15:12:19 +0200

    add google auth

--- src/chesslessons/components/sign_in.components.cljs
+++ src/chesslessons/components/sign_in.components.cljs
@@ -24,9 +24,11 @@
 	  [:input.form-control {:placeholder "Password" :required true :type "password"}]
 	  ]
 
+	 [:button.btn.btn-primary {:on-click sign_in-model/toggle} "toggle"]
+
 	 [:div.form-group {:style {:display (if @sign_in-model/is_button_visible "block" "none")}}
 	  [:img {:src "https://i.stack.imgur.com/ZW4QC.png" :onClick fbs/facebook_auth :style {:cursor "pointer"} }]
+	  [:img {:src "https://developers.google.com/+/images/branding/sign-in-buttons/Red-signin_Google_base_44dp.png" :onClick fbs/google_auth :style {:cursor "pointer" :height 60} }]
 	  ]
-	 [:button.btn.btn-primary {:on-click sign_in-model/toggle} "toggle"]
 	 ]
 	)
