8005deb850bf56f101e2df030c0b283bc8bf4cad
malyutenko.d <malyutenko.d@gmail.com>
2018-02-07 14:07:00 +0200

    add hide button

--- src/chesslessons/components/sign_in.components.cljs
+++ src/chesslessons/components/sign_in.components.cljs
@@ -1,42 +1,32 @@
 (ns chesslessons.components.sign_in.components
 	(:require
 		[chesslessons.firebase :as fbs :refer [firebase]]
+		;       Models
+		[chesslessons.sign_in-model :as sign_in-model]
 ))
 
 
 (def log (.-log js/console))
 
 
-; ==================
-; Atoms
-(def is_button_visible (atom nil))
-
-
-(defn toggle [e]
-	(.preventDefault e)
-	(reset! is_button_visible true)
-	(log "click3" @is_button_visible)
-	)
-
-
 (defn render []
-	(log @is_button_visible "is_button_visible")
+	(log @sign_in-model/is_button_visible "is_button_visible")
 	[:form
 	 [:h1.h3.mb-3.font-weight-normal "Please sign in"]
-	 
+
 	 [:div.form-group
 	  [:label.sr-only "Email address"]
 	  [:input.form-control {:placeholder "Email address" :required true :type "email"}]
 	  ]
-	
+
 	 [:div.form-group
 	  [:label.sr-only "Password"]
 	  [:input.form-control {:placeholder "Password" :required true :type "password"}]
 	  ]
-	 
-	 [:div.form-group {:style {:display (if @is_button_visible "block" "none")}}
+
+	 [:div.form-group {:style {:display (if @sign_in-model/is_button_visible "block" "none")}}
 	  [:img {:src "https://i.stack.imgur.com/ZW4QC.png" :onClick fbs/facebook_auth :style {:cursor "pointer"} }]
 	  ]
-	 
-	 [:button.btn.btn-primary {:on-click toggle} "Sign in"]
-	 ])
+	 [:button.btn.btn-primary {:on-click sign_in-model/toggle} "toggle"]
+	 ]
+	)
