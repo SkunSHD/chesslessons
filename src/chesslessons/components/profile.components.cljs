35b4b5b772a003a783838732d280059374c789e1
malyutenko.d <malyutenko.d@gmail.com>
2018-02-26 15:50:56 +0200

    Add alert

--- src/chesslessons/components/profile.components.cljs
+++ src/chesslessons/components/profile.components.cljs
@@ -2,20 +2,28 @@
 	(:require
 ;       Models
 		[chesslessons.visitor-model :refer [visitor, logout]]
+;       Utils
+		[goog.string :as gstring]
+		[clojure.string :as str]
 		))
 
 (def log (.-log js/console))
 (defn cljs [string] (js->clj (.parse js/JSON string) :keywordize-keys true))
 
 
+(defn visitor_name []
+	(first (str/split (:name @visitor) #"\s" )))
+
 
 (defn render []
 	[:div
-	  [:div
+	  [:div.alert.alert-warning.alert-dismissible.fade.show {:role "alert"}
+	   "Hi, " [:strong (visitor_name)] "! I'm gonna send you a massage or something in closest future!"
+	   [:button.close {:type "button" :data-dismiss "alert" :aria-label "Close"}
+	   	[:span {:aria-hidden "true"} (gstring/unescapeEntities "&times;")]]]
+
 	   [:a {:href (:link @visitor)}
-            [:img { :width 150 :height 150 :src (:photo @visitor)}]
-            [:h3 "User name:" (:name @visitor)]
-	    ]]
-        [:h6 "Email: " (:email@visitor)]
-        [:h6 "Gender: " (:gender @visitor)]
+			 [:img { :width 150 :height 150 :src (:photo @visitor)}]
+			 [:h3 "User name:" (:name @visitor)]]
+		 [:h6 "Email: " (:email@visitor)]
 	 ])
